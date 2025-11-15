e522f686541b51702dbbb09ef52bbf09bd7ba4df
Mohammed Aladl <aladlmuhammed97@gmail.com>
2025-11-15 15:33:31 +0200

    feat: add promise.all to stats fetching for faster execution

--- backend/src/controllers/slot/getSlotsStats.ts
+++ backend/src/controllers/slot/getSlotsStats.ts
@@ -1,5 +1,4 @@
 import asyncHandler from "express-async-handler";
-
 import { success, fail, prisma, userSelect as providerSelect } from "../../lib";
 
 export const getProviderSlotsStats = asyncHandler(
@@ -16,25 +15,25 @@ export const getProviderSlotsStats = asyncHandler(
 
     if (!provider) return fail("Provider not found", 404);
 
-    const totalSlots = await prisma.slot.count({
-      where: {
-        userId: providerId,
-      },
-    });
-
-    const availableSlots = await prisma.slot.count({
-      where: {
-        userId: providerId,
-        status: "AVAILABLE",
-      },
-    });
-
-    const bookedSlots = await prisma.slot.count({
-      where: {
-        userId: providerId,
-        status: "BOOKED",
-      },
-    });
+    const [totalSlots, availableSlots, bookedSlots] = await Promise.all([
+      prisma.slot.count({
+        where: {
+          userId: providerId,
+        },
+      }),
+      prisma.slot.count({
+        where: {
+          userId: providerId,
+          status: "AVAILABLE",
+        },
+      }),
+      prisma.slot.count({
+        where: {
+          userId: providerId,
+          status: "BOOKED",
+        },
+      }),
+    ]);
 
     success(res, {
       provider,
